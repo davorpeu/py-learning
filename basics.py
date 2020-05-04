0b60b52fd7536534a9c9d205e71572fe159cd892
dadop <davorpeu@gmail.com>
2020-05-04 22:21:33 +0200

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,27 @@
+num = 5
+print(num)
+num1 = 5.1
+print(num1)
+type(num1)
+is_true = True
+print(is_true)
+notype = None
+print(notype)
+a = 6
+b = 2
+c = a + b
+print(c)
+d = a-b
+print (d)
+e = a * b
+print(e)
+f = a / b
+print(f)
+g = a ** b
+print(g)
+
+ab = 5
+ac = 2
+
+ad = ab > ac 
+print(ad)
